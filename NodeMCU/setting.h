c15897e6e07cc3f31a75de23bde66adf2b892075
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-07-05 01:49:48 +0530

    Add files via upload

---
+++ NodeMCU/setting.h
@@ -0,0 +1,14 @@
+#ifndef setting_h
+    #define setting_h
+    
+    /************* wifi setting *******************/
+    #define SSID        "Official Black Knight"
+    #define PASSWORD    "ZigmaE#4O4"
+
+    /************* Server Setting ****************/
+    #define API               "http://192.168.8.110/systemapitest2/dbwrite.php"
+    #define API_KEY           "tPmAT5Ab3j7F9"
+    #define ERROR_MSG         "{\"success\":\"false\",\"massage\":\"error\"}"
+    #define ERROR_MSG_WIFI    "{\"success\":\"false\",\"massage\":\"wifi not connected\"}"
+    #define SERIAL_WAITING_TIME 2000
+#endif
